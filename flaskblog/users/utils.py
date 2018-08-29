41dd290dc920e030657d83213ae79df585627a2e
andre_vauvelle <vauvelleandre@gmail.com>
2018-08-29 21:23:14 +0100

    finished tutorials

--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -1,9 +1,17 @@
 
+import os
+import secrets
+from PIL import Image
+from flask import url_for, current_app
+from flask_mail import Message
+from flaskblog import mail
+
+
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
 
     output_size = (125, 125)
     i = Image.open(form_picture)
@@ -18,7 +26,7 @@ def send_reset_email(user):
                   sender='noreply@demo.com',
                   recipients=[user.email])
     msg.body = f'''To reset your password, visit the following link:
-{url_for('reset_token', token=token, _external=True)}
+{url_for('users.reset_token', token=token, _external=True)}
 
 if you did not make this request then simply ignore this email and no change is needed.    
 '''
